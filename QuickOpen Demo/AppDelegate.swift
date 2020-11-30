8a2e08394627c09f51bb994b0ddecef94fa6af49
oscardoudou <oscar9591@icloud.com>
2020-11-29 20:46:06 -0500

    Initial Commit

---
+++ QuickOpen Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickOpen Demo
+//
+//  Created by 张壹弛 on 11/29/20.
+//  Copyright © 2020 oscardoudou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuickOpen Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuickOpen Demo
+//
+//  Created by 张壹弛 on 11/29/20.
+//  Copyright © 2020 oscardoudou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
