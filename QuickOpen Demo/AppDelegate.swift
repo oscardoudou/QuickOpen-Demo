ebf1f4526549b1ba3e37aec273eaf02023ab4b7d
oscardoudou <oscar9591@icloud.com>
2020-11-29 21:24:52 -0500

    add quick open as submodule of demo

--- QuickOpen Demo/AppDelegate.swift
+++ QuickOpen Demo/AppDelegate.swift
@@ -7,20 +7,72 @@
 //
 
 import Cocoa
+import Carbon
+import quickopen
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var quickOpenWindowController: QuickOpenWindowController!
+    
+    func register() {
+          var hotKeyRef: EventHotKeyRef?
+          let modifierFlags: UInt32 = UInt32(optionKey)
+            
+          let keyCode = kVK_Space
+          var gMyHotKeyID = EventHotKeyID()
 
+          gMyHotKeyID.id = UInt32(keyCode)
+          gMyHotKeyID.signature = OSType("swat".fourCharCodeValue)
 
+          var eventType = EventTypeSpec()
+          eventType.eventClass = OSType(kEventClassKeyboard)
+          eventType.eventKind = OSType(kEventHotKeyReleased)
+            
+            let observer = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+          // Install handler.
+          InstallEventHandler(GetApplicationEventTarget(), {
+            (nextHanlder, theEvent, observer) -> OSStatus in
+            let mySelf = Unmanaged<AppDelegate>.fromOpaque(observer!).takeUnretainedValue()
+            mySelf.quickOpenWindowController.toggle()
+            print("Option +  Space Released!")
+            return noErr
+          }, 1, &eventType, observer, nil)
+
+          // Register hotkey.
+          let status = RegisterEventHotKey(UInt32(keyCode),
+                                           modifierFlags,
+                                           gMyHotKeyID,
+                                           GetApplicationEventTarget(),
+                                           0,
+                                           &hotKeyRef)
+          assert(status == noErr)
+        }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        register()
+        
+        NSApp.windows.forEach{window in
+            if(window.className.contains("NSWindow")){
+                window.close()
+            }
+        }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
 
+extension String{
+    public var fourCharCodeValue: Int {
+      var result: Int = 0
+      if let data = self.data(using: String.Encoding.macOSRoman) {
+        data.withUnsafeBytes({ (rawBytes) in
+          let bytes = rawBytes.bindMemory(to: UInt8.self)
+          for i in 0 ..< data.count {
+            result = result << 8 + Int(bytes[i])
+          }
+        })
+      }
+      return result
+    }
+}
--- QuickOpen Demo/ViewController.swift
+++ QuickOpen Demo/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import Cocoa
+import quickopen
 
 class ViewController: NSViewController {
-
+    private var quickOpenWindowController:QuickOpenWindowController!
+    var appDelegate: AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let search = QuickOpenSearch()
+        search.width = 500
+        search.height = 50
+//        search.delegate = self
+        search.persistPosition = true
+        search.placeholder = "Dummy"
+        
+        quickOpenWindowController = QuickOpenWindowController(search: search )
+        
+        appDelegate = NSApplication.shared.delegate as! AppDelegate
+        appDelegate.quickOpenWindowController = quickOpenWindowController
+    
     }
 
     override var representedObject: Any? {
@@ -24,4 +39,3 @@ class ViewController: NSViewController {
 
 
 }
-
